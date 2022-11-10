cbf68553e95f3f4be6ba380b8f4fe010c5ff8279
Yun Chen <Yunchenfly@gmail.com>
2022-11-10 00:32:58 -0500

    commit tree 496461d233e0be50d0ff8360e1046125b05fdda2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let message = []
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return message
+
+}
+
+function countDown(num) {
+    while (num >= 0) 
+        console.log(num--)
+    
+}
